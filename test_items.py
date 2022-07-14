6317ea31c6fc2cdd1d09860df727603742f1d5c3
NikichMg <107757742+NikichMg@users.noreply.github.com>
2022-07-14 19:31:46 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,6 +1,20 @@
+import time
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_link_button(browser):
+
+def test_check_add_to_basket_button(browser: webdriver.Chrome):
+    print("start test")
     browser.get(link)
-    browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']").click()
-    
+    # time.sleep(30)
+    browser.implicitly_wait(10)
+    try:
+        button = browser.find_element(By.CLASS_NAME, "btn-add-to-basket")
+        print(f"button text = {button.text}")
+    except NoSuchElementException:
+        assert False, 'button has not find'
+    print("finish test1")
