f6acb8df184a9f70ed1bb8aa47471d8ff1777739
NikichMg <107757742+NikichMg@users.noreply.github.com>
2022-07-14 19:03:43 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -1,12 +1,6 @@
-from selenium import webdriver
-import pytest
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-
-class TestCase:
-    def test_add_to_basket_button(self, browser, language):
-        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-        browser.get(link)
-
-        button_text = browser.find_by_css_selector(
-            '[value="Add to basket"]').text
-        assert button_text == "Add to basket"
+def test_guest_should_see_link_button(browser):
+    browser.get(link)
+    browser.find_element_by_xpath("//button[@class='btn btn-lg btn-primary btn-add-to-basket']").click()
+    
