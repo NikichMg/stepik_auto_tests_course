561c0a89d2a7361ac1d8cf24518572ab67790884
NikichMg <107757742+NikichMg@users.noreply.github.com>
2022-07-14 18:55:26 +0500

    Create conftest.py

---
+++ conftest.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language_name', action='store', default=None,
+                     help="Choose language: ru or en")
+
+
+
+@pytest.fixture(scope="function")
+def language(request):
+    language_name = request.config.getoption("language_name")
+    if language_name == "es":
+        language_name = "es"
+    elif language_name == "en":
+        language_name = "en-gb"
+    link = f"http://selenium1py.pythonanywhere.com/{language_name}/catalogue/coders-at-work_207/"
+    browser.get(link)
+
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart chrome browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
